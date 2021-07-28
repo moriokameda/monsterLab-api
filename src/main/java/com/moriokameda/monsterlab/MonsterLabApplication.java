e8acac9eee1d848a567052de87be28734061257b
Morio Kameda <mkame19941031@gmail.com>
2021-07-29 00:53:57 +0900

    first commit

---
+++ src/main/java/com/moriokameda/monsterlab/MonsterLabApplication.java
@@ -0,0 +1,13 @@
+package com.moriokameda.monsterlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moriokameda/monsterlab/MonsterLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moriokameda.monsterlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
